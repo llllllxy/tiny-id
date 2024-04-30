baa18fcb32f2657a73211003ab030800d83afbd3
llllllxy <34022230+llllllxy@users.noreply.github.com>
2024-04-30 14:43:43 +0800

    完善表结构

--- src/main/resources/init/tiny-id.sql
+++ src/main/resources/init/tiny-id.sql
@@ -56,7 +56,7 @@ CREATE TABLE `t_idtable`  (
   `id_code` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '业务流水号标识编码',
   `id_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '业务流水号名称',
   `id_value` bigint(20) NOT NULL DEFAULT 0 COMMENT '业务流水号当前值',
-  `id_step` int(11) NULL DEFAULT NULL COMMENT '步长',
+  `id_step` int(11) NOT NULL DEFAULT 100 COMMENT '步长',
   `id_length` int(11) NOT NULL COMMENT '业务流水号长度',
   `has_prefix` tinyint(4) NOT NULL COMMENT '是否有前缀 1有 0无',
   `id_prefix` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '业务流水号前缀内容',
@@ -90,26 +90,8 @@ CREATE TABLE `t_worker_node`  (
   `updated_at` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
   `signed_at` datetime(0) NULL DEFAULT NULL COMMENT '记录时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `worker_datacenter_unique_index`(`worker_id`, `datacenter_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '反馈建议表' ROW_FORMAT = Dynamic;
+  UNIQUE INDEX `worker_datacenter_unique_index`(`worker_id`, `datacenter_id`) USING BTREE COMMENT '联合唯一索引'
+) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '服务节点表' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of t_worker_node
--- ----------------------------
-INSERT INTO `t_worker_node` VALUES (1, '192.168.0.107', 9999, 21, 2, '2024-04-27 13:51:38', '2024-04-27 13:51:38', '2024-04-27 13:51:39');
-INSERT INTO `t_worker_node` VALUES (2, '192.168.0.107', 9999, 20, 2, '2024-04-27 13:56:34', '2024-04-27 13:56:34', '2024-04-27 13:56:34');
-INSERT INTO `t_worker_node` VALUES (3, '192.168.0.107', 9999, 5, 2, '2024-04-27 14:09:58', '2024-04-27 14:09:58', '2024-04-27 14:09:59');
-INSERT INTO `t_worker_node` VALUES (4, '192.168.0.107', 9999, 16, 2, '2024-04-27 14:13:51', '2024-04-27 14:13:51', '2024-04-27 14:13:51');
-INSERT INTO `t_worker_node` VALUES (5, '192.168.0.107', 9999, 0, 2, '2024-04-27 15:33:33', '2024-04-27 16:36:33', '2024-04-27 16:36:34');
-INSERT INTO `t_worker_node` VALUES (6, '192.168.0.107', 9999, 17, 2, '2024-04-27 16:36:58', '2024-04-27 16:39:58', '2024-04-27 16:39:59');
-INSERT INTO `t_worker_node` VALUES (7, '192.168.0.107', 9999, 30, 2, '2024-04-27 16:40:34', '2024-04-27 20:53:49', '2024-04-27 20:53:49');
-INSERT INTO `t_worker_node` VALUES (8, '192.168.0.107', 9999, 23, 2, '2024-04-27 20:54:09', '2024-04-27 21:07:09', '2024-04-27 21:07:10');
-INSERT INTO `t_worker_node` VALUES (9, '192.168.0.107', 9999, 11, 2, '2024-04-27 21:07:50', '2024-04-27 21:07:50', '2024-04-27 21:07:50');
-INSERT INTO `t_worker_node` VALUES (10, '192.168.0.107', 9999, 29, 2, '2024-04-27 21:07:59', '2024-04-27 21:10:59', '2024-04-27 21:10:59');
-INSERT INTO `t_worker_node` VALUES (11, '192.168.0.107', 9999, 13, 2, '2024-04-27 21:13:02', '2024-04-27 21:16:02', '2024-04-27 21:16:03');
-INSERT INTO `t_worker_node` VALUES (12, '192.168.0.107', 9999, 7, 2, '2024-04-27 21:17:05', '2024-04-27 21:18:05', '2024-04-27 21:18:06');
-INSERT INTO `t_worker_node` VALUES (13, '192.168.0.107', 9999, 31, 2, '2024-04-27 21:18:25', '2024-04-27 21:19:25', '2024-04-27 21:19:26');
-INSERT INTO `t_worker_node` VALUES (14, '192.168.0.107', 9999, 24, 2, '2024-04-27 21:20:33', '2024-04-27 21:27:33', '2024-04-27 21:27:34');
-INSERT INTO `t_worker_node` VALUES (15, '192.168.0.107', 9999, 10, 2, '2024-04-27 21:29:02', '2024-04-27 22:46:02', '2024-04-27 22:46:03');
 
 SET FOREIGN_KEY_CHECKS = 1;
